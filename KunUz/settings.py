92756d44d1e670d5015ad36272932fc0cc217b98
shamsiddin808 <marsitschool456@gmail.com>
2025-04-26 22:11:36 +0500

    update allowed hosts

--- KunUz/settings.py
+++ KunUz/settings.py
@@ -136,8 +136,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Sizning development static fayllaringiz
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # collectstatic ularni shu yerga nusxalaydi
+
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
